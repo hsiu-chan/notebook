c92063df029b22cbb739d1face7d0a5964ed3713
chc <106435999+hsiu-chan@users.noreply.github.com>
2022-12-04 14:17:26 +0800

    nothing

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var sidebarTOCBtn = document.getElementById('sidebar-toc-btn')
+
+sidebarTOCBtn.addEventListener('click', function(event) {
+  event.stopPropagation()
+  if (document.body.hasAttribute('html-show-sidebar-toc')) {
+    document.body.removeAttribute('html-show-sidebar-toc')
+  } else {
+    document.body.setAttribute('html-show-sidebar-toc', true)
+  }
+})
