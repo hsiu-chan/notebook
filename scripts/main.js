5114317691fd641d08c108dd959bb6e8ee16e595
chc <106435999+hsiu-chan@users.noreply.github.com>
2022-12-14 11:12:52 +0800

    create 4 files and update 4 files

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,17 @@ sidebarTOCBtn.addEventListener('click', function(event) {
     document.body.setAttribute('html-show-sidebar-toc', true)
   }
 })
+
+var idxs= document.getElementById('tt')
+
+//const folder_path ='./notes/'
+
+document.getElementById('my_test').addEventListener('change', function(event) {	//my_test 為觸發運作的按鈕
+	let files = event.target.files;	//選取之目標目錄內的檔案清單
+	let result=''	//(透過id)取得要放置圖片的標籤位置
+	for(let i=0; i<files.length; i++) {
+		let item = document.createElement("img");	//產生圖片標籤
+		result+=files[i].webkitRelativePath;	//設定圖片的來源位置
+	}
+  idxs.textContent=result;
+});
